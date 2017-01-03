31fa0ae1bc1e8fb0f91c76b8db415c111aaede91
Andrii Mutylo <amutylo@gmail.com>
2017-01-03 19:05:08 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+let currentYear = new Date().getFullYear();
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let logString = `${now}, ${req.method} ${req.url}` + '\n';
+
+    fs.appendFile('server.log', logString, (err) => {
+        if (err) {
+            console.log('Can not write log string to a file.');
+        }
+    })
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+       res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: "Welcome to the home page"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page'
+    });
+})
+app.listen(3000, () => {
+    console.log('Server up on port 3000');
+});
