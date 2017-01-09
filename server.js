bd88b54371e380afbb983611cf94719f7f98e33b
Andrii Mutylo <amutylo@gmail.com>
2017-01-09 16:37:52 +0200

    change into environment variables PORT

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000
 
 let app = express();
 let currentYear = new Date().getFullYear();
@@ -36,6 +37,6 @@ app.get('/about', (req, res) => {
         pageTitle: 'About page'
     });
 })
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server up on port 3000');
 });
