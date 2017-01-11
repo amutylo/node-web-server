364699b0f61c32ad3699a722a9920dbb52d68b40
Andrii Mutylo <amutylo@gmail.com>
2017-01-11 21:55:27 +0200

    nstr

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
        res.render('home.hbs', {
         pageTitle: 'Home page',
-        welcomeMessage: "Welcome to the home page"
+        welcomeMessage: "Welcome to the home page",
     });
 });
 
